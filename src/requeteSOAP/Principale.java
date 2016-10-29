7a7e2b35d9fe041b3184c62a66da6a8fdb91b723
Gaëtan <gaetan.roche@etu.univ-lyon1.fr>
2016-10-29 10:55:35 +0200

    premier commit version prof

---
+++ src/requeteSOAP/Principale.java
@@ -0,0 +1,24 @@
+package requeteSOAP;
+
+public class Principale {
+
+	private static String operation = "getTousLesPays";
+	private static String destenvoi = "http://localhost:8080/PWSPays/services/WebServices";
+	private static String destination = "http://service"; // Nom du package
+
+
+	private static EnvoiMessageSOAP unAppel = new EnvoiMessageSOAP();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			unAppel.connexion();
+			unAppel.creationMessageListePays();
+			unAppel.emissionReceptionUnPays();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
